f7fd01ec179df38aa5f208e186f6564f1a1d3fdc
LycheBoy <92761042+LycheBoy@users.noreply.github.com>
2021-10-20 11:44:32 -1000

    First Commit
    added 2 HTML files, and a js file

---
+++ script.js
@@ -0,0 +1,15 @@
+/* This is the first step in building a complete we applications.
+
+Why use Local Storage over browser cookies?.
+
+Local storage bypasses web browser cookie blockers.
+
+Local storage area(Over 5mb) compared to cookies(4kb).
+
+Local Storage is more secure than cookies because the data is stored on the user's computer and not transmitted to the server.
+
+Local Storage can set to expire after he session is complete
+
+Local Storage is a way to pass values from one page o another because JavaScript clears all variables
+*/
+
